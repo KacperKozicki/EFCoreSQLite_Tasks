5a9092778ccd23cba6f7ad30426310cfd7e6b1be
KacperKozicki <kacper.kozicki@microsoft.wsei.edu.pl>
2023-06-15 04:06:16 +0200

    Dodano klase Users.cs

---
+++ EFCoreSQLite_Tasks/Models/Users.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Tasks.Models
+{
+    public class Users
+    {
+        /// <summary>
+        /// Pobiera lub ustawia identyfikator użytkownika.
+        /// </summary>
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia imię użytkownika.
+        /// </summary>
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia nazwisko użytkownika.
+        /// </summary>
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia hasło użytkownika.
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia identyfikator roli użytkownika.
+        /// </summary>
+        public int RoleId { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia rolę użytkownika.
+        /// </summary>
+        public Role Role { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia zadania przypisane do użytkownika.
+        /// </summary>
+        public ICollection<Tasks> Tasks { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia postępy zadań użytkownika.
+        /// </summary>
+        public ICollection<TaskProgress> TaskProgress { get; set; }
+    }
+}
+
