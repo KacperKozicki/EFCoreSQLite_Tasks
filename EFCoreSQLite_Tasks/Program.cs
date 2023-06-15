4e8808c979e80d08ed4e239e413bb8fa4a9ad5bb
KacperKozicki <115461991+KacperKozicki@users.noreply.github.com>
2023-06-15 03:56:33 +0200

    Dodaj pliki projektów.

---
+++ EFCoreSQLite_Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreSQLite_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
