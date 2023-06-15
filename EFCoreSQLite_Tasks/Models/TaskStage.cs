617a286ce62bd75dbdfa054c39718e79ac1f0b98
KacperKozicki <kacper.kozicki@microsoft.wsei.edu.pl>
2023-06-15 04:06:01 +0200

    Dodano klasę TaskStage.cs

---
+++ EFCoreSQLite_Tasks/Models/TaskStage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Tasks.Models
+{
+    public class TaskStage
+    {
+        /// <summary>
+        /// Pobiera lub ustawia identyfikator etapu zadania.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia nazwę etapu zadania.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia zadania powiązane z etapem.
+        /// </summary>
+        public List<Tasks> Tasks { get; set; }
+    }
+}
+
