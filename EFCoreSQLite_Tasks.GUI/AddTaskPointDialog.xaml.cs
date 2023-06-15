b002a3201b3bbd69f4f3a3f6b3144f66ffbca67c
KacperKozicki <kacper.kozicki@microsoft.wsei.edu.pl>
2023-06-15 04:16:50 +0200

    Dodano AddTaskspointDialog, korekta przestrzeni nazw

---
+++ EFCoreSQLite_Tasks.GUI/AddTaskPointDialog.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using EFCore_Tasks.DataAccess;
+using EFCore_Tasks.Models;
+
+namespace EFCore_Tasks.GUI
+{
+    public partial class AddTaskPointDialog : Window
+    {
+        private TaskPoint newTaskPoint; 
+        private readonly TaskContext taskContext;
+        private readonly TaskListWindow taskListWindow;
+        private readonly Tasks currentTask;
+        public AddTaskPointDialog(TaskListWindow taskListWindow, Tasks currentTask)
+        {
+            InitializeComponent();
+            taskContext = new TaskContext();
+            this.taskListWindow = taskListWindow; 
+            this.currentTask = currentTask;
+
+        }
+       
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            TaskPoint newTaskPoint = new TaskPoint
+            {
+                Content = TaskTitleTextBox.Text,
+                IsCompleted = false,
+                TaskId=currentTask.Id,
+            };
+
+            // Dodawanie nowego zadania do głównego okna listy zadań
+
+
+            // Dodaj nowy taskpoint do zadania
+            taskListWindow.AddNewTaskPoint(newTaskPoint);
+            Close();
+           
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Zamknij okno dialogowe bez zapisywania nowego TaskPointa
+            this.Close();
+        }
+
+       
+    }
+}
