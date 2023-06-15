83a4c285e3209a4b249f3913dc1ee6c7057a4051
KacperKozicki <kacper.kozicki@microsoft.wsei.edu.pl>
2023-06-15 04:04:43 +0200

    Dodano klasę TaskPriority.cs

---
+++ EFCoreSQLite_Tasks/Models/TaskPriority.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Tasks.Models
+{
+    public class TaskPriority
+    {
+        /// <summary>
+        /// Pobiera lub ustawia identyfikator priorytetu zadania.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia nazwę priorytetu zadania.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Pobiera lub ustawia kolekcję zadań.
+        /// </summary>
+        public ICollection<Tasks> Tasks { get; set; }
+    }
+}
+
